475066970726a4a66b4d10ce23d02f6dfbe21bd9
Matteo Santoro <m.santoro1997@gmail.com>
2023-03-28 10:23:15 +0200

    fix: lighthouse improvements

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const handleClick = () => {
 createRoot(app).render(
   <>
     <nav className="bg-pastel-blue h-20 overflow-hidden justify-center flex shadow-xl fixed w-screen">
-      <img src={require('./assets/Logo.webp')} alt="website logo" className='h-20 mix-blend-multiply' />
+      <img src={require('./assets/logo.svg')} alt="website logo" className='h-20 mix-blend-multiply' width={250} height={150} />
     </nav>
 
     <div className="min-h-screen bg-gray-200 max-w-7xl mx-auto pt-20">
@@ -46,12 +46,12 @@ createRoot(app).render(
         </div>
 
         <div className='mb-8 '>
-          <h2 class="text-3xl font-bold mb-2">Skills e competenze</h2>
+          <h2 className="text-3xl font-bold mb-2">Skills e competenze</h2>
           <hr className="mb-2 h-[6px] bg-blue-400 w-32" />
-          <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 text-gray-900">
-            <div class="bg-white shadow rounded-lg p-6">
-              <h3 class="text-lg font-bold mb-4">ReactJS</h3>
-              <p class="px-4">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 text-gray-900">
+            <div className="bg-white shadow rounded-lg p-6">
+              <h3 className="text-lg font-bold mb-4">ReactJS</h3>
+              <p className="px-4">
                 Con oltre 3 anni di esperienza professionale nello sviluppo web con ReactJS, ho acquisito competenze approfondite nel realizzare applicazioni web ad alte prestazioni.
                 Grazie all'utilizzo dei giusti Design Patterns, Hooks e Memoization, sono in grado di creare Single Page Application (SPA) che forniscono un'esperienza utente fluida e coinvolgente.
                 Durante la mia carriera, ho avuto il privilegio di utilizzare ReactJS in diversi progetti interessanti, quali
@@ -61,9 +61,9 @@ createRoot(app).render(
                 Se stai cercando un esperto in ReactJS per il tuo progetto, non esitare a contattarmi. Sono pronto a fornirti le mie competenze e l'esperienza necessarie per portare il tuo progetto al successo.
               </p>
             </div>
-            <div class="bg-white shadow rounded-lg p-6">
-              <h3 class="text-lg font-bold mb-4">TailwindCSS</h3>
-              <p class="px-4">
+            <div className="bg-white shadow rounded-lg p-6">
+              <h3 className="text-lg font-bold mb-4">TailwindCSS</h3>
+              <p className="px-4">
                 Con oltre 2 anni di esperienza professionale nell'utilizzo di TailwindCSS, ho acquisito competenze approfondite che mi permettono di integrare questo framework in piattaforme web già avviate, utilizzando tecnologie diverse o CSS personalizzati.
                 Grazie alla mia esperienza presso Vericast e Shopfully, ho sviluppato competenze avanzate nella personalizzazione di classi CSS e nell'estensione del framework Tailwind,
                 permettendomi di adattare facilmente il design alle esigenze specifiche di ciascun progetto.
@@ -72,9 +72,9 @@ createRoot(app).render(
                 La mia esperienza nell'utilizzo di TailwindCSS mi ha permesso di personalizzare facilmente le classi CSS e di adattarle alle specifiche esigenze del progetto.
               </p>
             </div>
-            <div class="bg-white shadow rounded-lg p-6">
-              <h3 class="text-lg font-bold mb-4">VueJS</h3>
-              <p class="px-4">
+            <div className="bg-white shadow rounded-lg p-6">
+              <h3 className="text-lg font-bold mb-4">VueJS</h3>
+              <p className="px-4">
                 Con 2 anni di esperienza professionale su VueJS, ho sviluppato competenze approfondite nell'utilizzo di questo framework moderno e avanzato.
                 Grazie ai miei sviluppi sulla piattaforma di gestione cashback e coupon per Vericast e nell'ecommerce di Deghi, ho ampliato la mia conoscenza di VueJS e Nuxt,
                 aprendomi a nuove possibilità e sfide. Come per il framework React, ho una vasta esperienza nell'implementazione di funzionalità e nell'utilizzo delle migliori
@@ -83,8 +83,8 @@ createRoot(app).render(
                 che mi permettono di sfruttare tutti i vantaggi che questa tecnologia offre.
               </p>
             </div>
-            <div class="bg-white shadow rounded-lg p-6">
-              <h3 class="text-lg font-bold mb-4">NodeJS</h3>
+            <div className="bg-white shadow rounded-lg p-6">
+              <h3 className="text-lg font-bold mb-4">NodeJS</h3>
               <p className='px-4'>
                 Ho oltre 5 anni di esperienza professionale nello sviluppo di backend su NodeJS. Fin dai miei primi giorni come sviluppatore, ho sviluppato una solida comprensione di come costruire e integrare API efficienti e performanti. Grazie alla mia esperienza in Fincons,
                 ho acquisito competenze nell'utilizzo di NodeJS per sviluppare Lambda Functions e applicazioni containerizzate su AWS, utilizzando framework come ExpressJS, Fastify e Nest.
@@ -94,8 +94,8 @@ createRoot(app).render(
                 Durante i miei sviluppi su Yoox, ho avuto modo di integrare dei bridge tra API già esistenti, affinando la mia conoscenza di alcuni design pattern specifici.
               </p>
             </div>
-            <div class="bg-white shadow rounded-lg p-6">
-              <h3 class="text-lg font-bold mb-4">Test e Time Estimation</h3>
+            <div className="bg-white shadow rounded-lg p-6">
+              <h3 className="text-lg font-bold mb-4">Test e Time Estimation</h3>
               <p className='px-4'>
                 Oltre alla mia esperienza nel backend su NodeJS, ho acquisito conoscenze avanzate nell'ambito dei test software durante il mio lavoro presso Vericast.
                 Ho migliorato il mio modo di scrivere test utilizzando il framework Mocha, e sono in grado di adottare il pattern di sviluppo TDD (Test Driven Development)
@@ -104,8 +104,8 @@ createRoot(app).render(
                 end-to-end per garantire la qualità del software.
               </p>
             </div>
-            <div class="bg-white shadow rounded-lg p-6">
-              <h3 class="text-lg font-bold mb-4">Altre Competenze</h3>
+            <div className="bg-white shadow rounded-lg p-6">
+              <h3 className="text-lg font-bold mb-4">Altre Competenze</h3>
               <p className='px-4'>
                 Durante la mia carriera da programmatore professionista, ho acquisito competenze fondamentali come l'utilizzo di strumenti di versionamento come Git,
                 sia attraverso la linea di comando che tramite GUI come SourceTREE. Sono in grado di applicare correttamente il pattern GitFlow e di gestire il versionamento su
@@ -119,55 +119,55 @@ createRoot(app).render(
           </div>
         </div>
         <div className='container mx-auto'>
-          <div class="mb-8">
-            <h2 class="text-3xl font-bold mb-2">Progetti</h2>
+          <div className="mb-8">
+            <h2 className="text-3xl font-bold mb-2">Progetti</h2>
             <hr className="mb-2 h-[6px] bg-blue-400 w-32" />
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
               <a href="https://www.yoox.com" target="_blank" rel="noopener noreferrer">
-                <div class="bg-white shadow rounded-lg overflow-hidden">
-                  <img class="w-full" src={require('./assets/yoox-preview.webp')} alt="Anteprima yoox.it" className='object-cover h-64 w-full' />
-                  <div class="p-4">
-                    <h3 class="text-lg font-bold mb-2 truncate">Special Campaigns | Yoox.it (employee)</h3>
+                <div className="bg-white shadow rounded-lg overflow-hidden">
+                  <img src={require('./assets/yoox-preview.webp')} alt="Anteprima yoox.it" className='object-cover h-64 w-full' />
+                  <div className="p-4">
+                    <h3 className="text-lg font-bold mb-2 truncate">Special Campaigns | Yoox.it (employee)</h3>
                   </div>
                 </div>
               </a>
               <a href="https://www.enel.it" target="_blank" rel="noopener noreferrer">
-                <div class="bg-white shadow rounded-lg overflow-hidden">
-                  <img class="w-full" src={require('./assets/enel-preview.webp')} alt="Anteprima viveresenzalattosio.it" className='object-cover h-64 w-full' />
-                  <div class="p-4">
-                    <h3 class="text-lg font-bold mb-2 truncate">WebApp Gestione Reclami | enel.it (Consultant)</h3>
+                <div className="bg-white shadow rounded-lg overflow-hidden">
+                  <img src={require('./assets/enel-preview.webp')} alt="Anteprima viveresenzalattosio.it" className='object-cover h-64 w-full' loading='lazy'/>
+                  <div className="p-4">
+                    <h3 className="text-lg font-bold mb-2 truncate">WebApp Gestione Reclami | enel.it (Consultant)</h3>
                   </div>
                 </div>
               </a>
               <a href="https://www.deghi.it" target="_blank" rel="noopener noreferrer">
-                <div class="bg-white shadow rounded-lg overflow-hidden">
-                  <img src={require('./assets/deghi-preview.webp')} alt="Anteprima deghi.ti" className='object-cover h-64 w-full' />
-                  <div class="p-4">
-                    <h3 class="text-lg font-bold mb-2">E-commerce | Deghi.it (external)</h3>
+                <div className="bg-white shadow rounded-lg overflow-hidden">
+                  <img src={require('./assets/deghi-preview.webp')} alt="Anteprima deghi.ti" className='object-cover h-64 w-full' loading='lazy'/>
+                  <div className="p-4">
+                    <h3 className="text-lg font-bold mb-2">E-commerce | Deghi.it (external)</h3>
                   </div>
                 </div>
               </a>
               <a href="https://www.doveconviene.it" target="_blank" rel="noopener noreferrer">
-                <div class="bg-white shadow rounded-lg overflow-hidden">
-                  <img class="w-full" src={require('./assets/doveconviene-preview.webp')} alt="Anteprima doveconviene.it" className='object-cover h-64 w-full' />
-                  <div class="p-4">
-                    <h3 class="text-lg font-bold mb-2 truncate">Campaign Manager | doveconviene.it (employee)</h3>
+                <div className="bg-white shadow rounded-lg overflow-hidden">
+                  <img src={require('./assets/doveconviene-preview.webp')} alt="Anteprima doveconviene.it" className='object-cover h-64 w-full' loading='lazy' />
+                  <div className="p-4">
+                    <h3 className="text-lg font-bold mb-2 truncate">Campaign Manager | doveconviene.it (employee)</h3>
                   </div>
                 </div>
               </a>
               <a href="https://www.ingenico.com" target="_blank" rel="noopener noreferrer">
-                <div class="bg-white shadow rounded-lg overflow-hidden">
-                  <img class="w-full" src={require('./assets/ingenico-preview.webp')} alt="Anteprima doveconviene.it" className='object-cover h-64 w-full' />
-                  <div class="p-4">
-                    <h3 class="text-lg font-bold mb-2 truncate">Tickets App | ingenico.com (consultant)</h3>
+                <div className="bg-white shadow rounded-lg overflow-hidden">
+                  <img src={require('./assets/ingenico-preview.webp')} alt="Anteprima doveconviene.it" className='object-cover h-64 w-full' loading='lazy'/>
+                  <div className="p-4">
+                    <h3 className="text-lg font-bold mb-2 truncate">Tickets App | ingenico.com (consultant)</h3>
                   </div>
                 </div>
               </a>
               <a href="https://www.viveresenzalattosio.it" target="_blank" rel="noopener noreferrer">
-                <div class="bg-white shadow rounded-lg overflow-hidden">
-                  <img class="w-full" src={require('./assets/viveresenzalattosio-preview.webp')} alt="Anteprima viveresenzalattosio.it" className='object-cover h-64 w-full' />
-                  <div class="p-4">
-                    <h3 class="text-lg font-bold mb-2">Blog | Viveresenzalattosio.it (Owner)</h3>
+                <div className="bg-white shadow rounded-lg overflow-hidden">
+                  <img src={require('./assets/viveresenzalattosio-preview.webp')} alt="Anteprima viveresenzalattosio.it" className='object-cover h-64 w-full' loading='lazy' />
+                  <div className="p-4">
+                    <h3 className="text-lg font-bold mb-2">Blog | Viveresenzalattosio.it (Owner)</h3>
                   </div>
                 </div>
               </a>
@@ -175,48 +175,48 @@ createRoot(app).render(
           </div>
         </div>
         <div className='container mx-auto'>
-          <h2 class="text-3xl font-bold mb-2">Contatti</h2>
+          <h2 className="text-3xl font-bold mb-2">Contatti</h2>
           <hr className="mb-2 h-[6px] bg-blue-400 w-32" />
-          <div class="flex flex-wrap bg-gray-100 p-5 rounded-lg shadow">
-            <div class="w-full md:w-1/2 px-4 mb-8">
-              <h3 class="text-lg font-bold mb-2">Email</h3>
-              <p class="text-gray-600">admin@msantoro.com</p>
+          <div className="flex flex-wrap bg-gray-100 p-5 rounded-lg shadow">
+            <div className="w-full md:w-1/2 px-4 mb-8">
+              <h3 className="text-lg font-bold mb-2">Email</h3>
+              <p className="text-gray-600">admin@msantoro.com</p>
             </div>
 
-            <div class="w-full md:w-1/2 px-4 mb-8">
-              <h3 class="text-lg font-bold mb-2">Telefono</h3>
-              <p class="text-gray-600">+39 3337360501</p>
+            <div className="w-full md:w-1/2 px-4 mb-8">
+              <h3 className="text-lg font-bold mb-2">Telefono</h3>
+              <p className="text-gray-600">+39 3337360501</p>
             </div>
 
-            <div class="w-full md:w-1/2 px-4 mb-8">
-              <h3 class="text-lg font-bold mb-2">Curriculum</h3>
-              <a href={require('./assets/CV.pdf')} target="_blank" class="text-blue-500 hover:underline"><i class="fas fa-file-pdf mr-2"></i> Scarica il mio curriculum</a>
+            <div className="w-full md:w-1/2 px-4 mb-8">
+              <h3 className="text-lg font-bold mb-2">Curriculum</h3>
+              <a href={require('./assets/CV.pdf')} target="_blank" className="text-gray-900 hover:underline"><i className="fas fa-file-pdf mr-2"></i> Scarica il mio curriculum</a>
             </div>
 
-            <div class="w-full md:w-1/2 px-4 mb-8">
-              <h3 class="text-lg font-bold mb-2">Social</h3>
-              <ul class="list-none">
-                <li class="mb-2">
-                  <a href="https://github.com/MatthewSaintbull" target="_blank" class="text-blue-500 hover:underline flex items-center">
-                    <i class="fab fa-github mr-2"></i>
+            <div className="w-full md:w-1/2 px-4 mb-8">
+              <h3 className="text-lg font-bold mb-2">Social</h3>
+              <ul className="list-none">
+                <li className="mb-2">
+                  <a href="https://github.com/MatthewSaintbull" target="_blank" className="text-gray-900 hover:underline flex items-center">
+                    <i className="fab fa-github mr-2"></i>
                     Github
                   </a>
                 </li>
-                <li class="mb-2">
-                  <a href="https://www.linkedin.com/in/matteo-santoro/" target="_blank" class="text-blue-500 hover:underline flex items-center">
-                    <i class="fab fa-linkedin mr-2"></i>
+                <li className="mb-2">
+                  <a href="https://www.linkedin.com/in/matteo-santoro/" target="_blank" className="text-gray-900 hover:underline flex items-center">
+                    <i className="fab fa-linkedin mr-2"></i>
                     LinkedIn
                   </a>
                 </li>
-                <li class="mb-2">
-                  <a href="https://www.instagram.com/matteo_santoro_97/" target="_blank" class="text-blue-500 hover:underline flex items-center">
-                    <i class="fab fa-instagram mr-2"></i>
+                <li className="mb-2">
+                  <a href="https://www.instagram.com/matteo_santoro_97/" target="_blank" className="text-gray-900 hover:underline flex items-center">
+                    <i className="fab fa-instagram mr-2"></i>
                     Instagram
                   </a>
                 </li>
-                <li class="mb-2">
-                  <a href="https://medium.com/@mttdev1997" target="_blank" class="text-blue-500 hover:underline flex items-center">
-                    <i class="fab fa-medium mr-2"></i>
+                <li className="mb-2">
+                  <a href="https://medium.com/@mttdev1997" target="_blank" className="text-gray-900 hover:underline flex items-center">
+                    <i className="fab fa-medium mr-2"></i>
                     Medium
                   </a>
                 </li>
@@ -227,7 +227,7 @@ createRoot(app).render(
       </div>
 
       <footer className="fixed bottom-0 left-0 right-0 bg-transparent p-4 flex justify-end">
-        <button className="bg-blue-200 text-white px-4 py-2 rounded-md" onClick={handleClick}>Scorri</button>
+        <button className="bg-pastel-blue text-gray-900 px-4 py-2 rounded-md shadow" onClick={handleClick}>Scorri</button>
       </footer>
     </div>
   </>
